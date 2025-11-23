3ab6e4c4c0bda3a8f96e6c1bd70d326a262faa0e
Elijah S Shmakov <gshkvbmstu@gmail.com>
2025-11-23 21:58:06 +0300

    edit AnimatedMac.css

--- src/components/AnimatedMac.js
+++ src/components/AnimatedMac.js
@@ -1,14 +1,17 @@
-// src/components/AnimatedMac.js
 import React from "react";
 import "./AnimatedMac.css";
 
-export default function AnimatedMac({ children }) {
+export default function AnimatedMac() {
   return (
     <div className="mac-container">
       <div className="mac-image-wrapper">
         <img src={process.env.PUBLIC_URL + "/mak.jpeg"} alt="MacIntosh" className="mac-image" />
         <div className="mac-screen">
-          {children}
+          <img
+            src={process.env.PUBLIC_URL + "/code.jpeg"}
+            alt="Вторая картинка внутри экрана"
+            className="mac-inside"
+          />
         </div>
       </div>
     </div>
