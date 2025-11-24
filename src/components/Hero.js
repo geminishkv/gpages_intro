30f25c8451dfbb1a784d24ca1d3fdd60ab78978b
Elijah S Shmakov <gshkvbmstu@gmail.com>
2025-11-24 14:02:06 +0300

    add typewritter.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,34 +1,32 @@
 import React from "react";
 import "./Hero.css";
+import Typewriter from "./Typewriter";
 import AnimatedMac from "./AnimatedMac";
 
 export default function Hero() {
   return (
-    <div className="hero">
-      <div className="hero-text">
-        <h1>
-          <span className="highlight">DevSecOps</span>
-        <br/><span className="highlight2">AppSec</span></h1>
-        <h2><br/>- Toolchain<br/>
-           - DevOps
-            <br />
-           - Infosec Risks 
-            <br />
-           - Стратегии и процессы
-            <br />
-           - PMI
-            <br />
-           - Кулуарный ИБ
-            <br />
-        </h2>
-      </div>
+      <div className="hero">
+        <div className="hero-text">
+          <h1>
+            <Typewriter text="DevSecOps" className="highlight" speed={70}/><br/>
+            <Typewriter text="AppSec" className="highlight2" speed={80}/>
+          </h1>
+          <h2>
+            <br />- Toolchain<br />
+            - DevOps<br />
+           - Infosec Risks<br />
+            - Стратегии и процессы<br />
+            - PMI<br />
+            - Кулуарный ИБ<br />
+          </h2>
+       </div>
       {/* В этот блок вставляйте вашу анимацию или любой JSX для отображения внутри "экрана" */}
-      <AnimatedMac>
+        <AnimatedMac>
         {/* Пример: внутри можно отобразить анимацию, изображение, gif или компонент */}
         {/* <CanvasAnimation /> */}
         {/* <img src={process.env.PUBLIC_URL + "/some_gif.gif"} alt="Anim" /> */}
         {/* <YourComponent /> */}
-      </AnimatedMac>
-    </div>
+        </AnimatedMac>
+     </div>
   );
 }
---
+++ src/components/Typewriter.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+export default function Typewriter({ text, className = "", speed = 80 }) {
+  const [displayed, setDisplayed] = useState("");
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setDisplayed(text.slice(0, i + 1));
+      i++;
+      if (i === text.length) clearInterval(interval);
+    }, speed);
+    return () => clearInterval(interval);
+  }, [text, speed]);
+  return <span className={className}>{displayed}</span>;
+}
