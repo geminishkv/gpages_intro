9814749e3ab112e533fce1171fe3fd4e0a027b4d
Elijah S Shmakov <gshkvbmstu@gmail.com>
2026-09-11 03:09:09 +0300

    chore(assets): drop unreferenced images, recompress the hero and Instagram covers
    Removes five orphaned hero/splash/logotype files and six Instagram covers that no
    post references. avatar.jpg goes from 593 KB to 105 KB, the hero cat becomes an
    86 KB WebP (was a 565 KB PNG) and Instagram covers are capped at 800 px wide.
    iCloud '* 2.*' duplicates were untracked and are simply deleted.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const LOGO_IMG  = process.env.PUBLIC_URL + '/img/logotype/logo_white.svg';
-export const UWU_IMG   = process.env.PUBLIC_URL + '/img/hero/uwu.png';
+export const UWU_IMG   = process.env.PUBLIC_URL + '/img/hero/uwu.webp';
 export const AVATAR_IMG = process.env.PUBLIC_URL + '/img/hero/avatar.jpg';
 export const LIDER_IMG = process.env.PUBLIC_URL + '/img/badges/lider.png';
 export const SBERSPASIBO_IMG = process.env.PUBLIC_URL + '/img/badges/sberspasibo.png';
